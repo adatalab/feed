41f2f6def5d81ff11541bb5b8c526a9755e8af37
YoungjunNa <ruminoreticulum@gmail.com>
2019-06-28 16:09:10 +0900

    Avg into avg

--- R/feed_compare.R
+++ R/feed_compare.R
@@ -16,20 +16,22 @@
 #' feed2 <- feed::feed_info("https://www.feedipedia.org/node/674", 3)
 #' feed1 <- feed1[[1]]
 #' feed2 <- feed2[[1]]
-#' feed_compare(feed1, feed2, name1 = "corn", name2 = "sbm", radar = TRUE)
-feed_compare <- function(feed1, feed2, name1 = "first", name2 = "second", radar = TRUE) {
-  feed1$Avg <- as.numeric(feed1$Avg)
-  feed2$Avg <- as.numeric(feed2$Avg)
+#' feed_compare(feed1, feed2, radar = TRUE)
+feed_compare <- function(feed1, feed2, radar = TRUE) {
+  feed1$avg <- as.numeric(feed1$avg)
+  feed2$avg <- as.numeric(feed2$avg)
 
   feed1 <- feed1[, c(1:3)]
   feed2 <- feed2[, c(1, 3)]
 
-  colnames(feed1) <- c("composition", "unit", name1)
-  colnames(feed2) <- c("composition", name2)
+  colnames(feed1) <- c("composition", "unit", "x")
+  colnames(feed2) <- c("composition", "y")
 
   join <- dplyr::left_join(feed1, feed2, by = "composition")
-  join$diff <- join[, 3] - join[, 4]
-  join$percentage <- round(join[, 4] / join[, 3], 3)
+  join <- mutate(join, diff = x - y, percentage = round(y/x*100, 1))
+  colnames(join)[3:4] <- c(as.character(quote(feed1)), as.character(quote(feed2)))
+  # join$diff <- join[, 3] - join[, 4]
+  # join$percentage <- round(join[, 4] / join[, 3], 3)
 
   print(join)
 
